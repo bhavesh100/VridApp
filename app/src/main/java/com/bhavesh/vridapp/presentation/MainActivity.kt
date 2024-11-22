b1ddbe5ceef684aa536922321b8fddf1a32a250a
Bhavesh Kumawat <kumawatbhavesh1000@gmail.com>
2024-11-22 20:09:50 +0530

    Adding navigation in main activity

--- app/src/main/java/com/bhavesh/vridapp/presentation/MainActivity.kt
+++ app/src/main/java/com/bhavesh/vridapp/presentation/MainActivity.kt
@@ -6,46 +6,39 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.bhavesh.vridapp.R
+import com.bhavesh.vridapp.presentation.blog_screen.BlogsScreen
 import com.bhavesh.vridapp.presentation.blog_screen.BlogsViewModel
+import com.bhavesh.vridapp.presentation.navigation.VridNavigation
 import com.bhavesh.vridapp.ui.theme.VridAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             VridAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopAppBar(title = {
+                        Text(text = stringResource(id = R.string.app_name))
+                    })}
+                ) { innerPadding ->
+                    VridNavigation(paddingValues = innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier , viewModel: BlogsViewModel = hiltViewModel()) {
-    Text(
-        text = "Hello $name!"+ viewModel.state.value.blogs.get(1),
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VridAppTheme {
-        Greeting("Android")
     }
 }
